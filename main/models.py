51635da5045131205b3acd2fd0f5d7ad2750cfcc
mma <111668501+mmalkindi@users.noreply.github.com>
2023-09-24 20:29:34 +0700

    tugas 4

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     amount = models.IntegerField(default=1)
     description = models.TextField()
